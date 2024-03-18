3c44e8642224c2cb8f498ba0600457a87a897336
Prasoon Sinha <prasoon.sinha@utexas.edu>
2024-03-18 20:38:06 +0000

    Add scripts to collect throughputs per job per mig slice

---
+++ collect_mig_3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+models=('squeezenet1_0' 'squeezenet1_1' 'efficientnet_b5' 'efficientnet_b6' 'efficientnet_b7')
+batches=(2 4 8 16 32)
+
+# First run with 4 and 3
+for model in "${models[@]}"; do
+    for batch in "${batches[@]}"; do
+        command="./run.sh --device-type a100 --device-id --modes mig --duration 60 --distribution closed vision-${model}-${batch} vision-${model}-${batch}"
+        echo "$command"
+        # eval $command
+    done
+done
+
+# Second run with 2, 2, 2, 1
+for model in "${models[@]}"; do
+    for batch in "${batches[@]}"; do
+        command="./run.sh --device-type a100 --device-id --modes mig --duration 60 --distribution closed vision-${model}-${batch} vision-${model}-${batch} vision-${model}-${batch} vision-${model}-${batch}"
+        echo "$command"
+        # eval $command
+    done
+done
