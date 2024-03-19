f964954a4238a2e933fd5a7bec015dc214e17cce
Prasoon Sinha <prasoon.sinha@utexas.edu>
2024-03-19 01:32:31 +0000

    Add -e to bash scripts, update exit_code variable in run.sh

--- collect_mig_0.sh
+++ collect_mig_0.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 models=('mobilenet_v2' 'mobilenet_v3_small' 'mobilenet_v3_large' 'densenet121' 'densenet161' 'densenet169')
 batches=(2 4 8 16 32)
@@ -6,17 +6,17 @@ batches=(2 4 8 16 32)
 # First run with 4 and 3
 for model in "${models[@]}"; do
     for batch in "${batches[@]}"; do
-        command="./run.sh --device-type a100 --device-id --modes mig --duration 60 --distribution closed vision-${model}-${batch} vision-${model}-${batch}"
+        command="./run.sh --device-type a100 --device-id 0 --modes mig --duration 60 --distribution closed vision-${model}-${batch} vision-${model}-${batch}"
         echo "$command"
-        # eval $command
+        eval $command
     done
 done
 
 # Second run with 2, 2, 2, 1
 for model in "${models[@]}"; do
     for batch in "${batches[@]}"; do
-        command="./run.sh --device-type a100 --device-id --modes mig --duration 60 --distribution closed vision-${model}-${batch} vision-${model}-${batch} vision-${model}-${batch} vision-${model}-${batch}"
+        command="./run.sh --device-type a100 --device-id 0 --modes mig --duration 60 --distribution closed vision-${model}-${batch} vision-${model}-${batch} vision-${model}-${batch} vision-${model}-${batch}"
         echo "$command"
-        # eval $command
+        eval $command
     done
 done
--- collect_mig_1.sh
+++ collect_mig_1.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 models=('densenet201' 'vgg11' 'vgg13' 'vgg16' 'vgg19' 'inception_v3')
 batches=(2 4 8 16 32)
@@ -6,17 +6,17 @@ batches=(2 4 8 16 32)
 # First run with 4 and 3
 for model in "${models[@]}"; do
     for batch in "${batches[@]}"; do
-        command="./run.sh --device-type a100 --device-id --modes mig --duration 60 --distribution closed vision-${model}-${batch} vision-${model}-${batch}"
+        command="./run.sh --device-type a100 --device-id 1 --modes mig --duration 60 --distribution closed vision-${model}-${batch} vision-${model}-${batch}"
         echo "$command"
-        # eval $command
+        eval $command
     done
 done
 
 # Second run with 2, 2, 2, 1
 for model in "${models[@]}"; do
     for batch in "${batches[@]}"; do
-        command="./run.sh --device-type a100 --device-id --modes mig --duration 60 --distribution closed vision-${model}-${batch} vision-${model}-${batch} vision-${model}-${batch} vision-${model}-${batch}"
+        command="./run.sh --device-type a100 --device-id 1 --modes mig --duration 60 --distribution closed vision-${model}-${batch} vision-${model}-${batch} vision-${model}-${batch} vision-${model}-${batch}"
         echo "$command"
-        # eval $command
+        eval $command
     done
 done
