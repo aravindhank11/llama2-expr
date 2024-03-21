483095d77abd54441e8288c4f668ba15a0465a45
Aravindhan Krishnan <aravindhank11@gmail.com>
2024-03-21 16:59:01 +0000

    Add scripts to collect training data for job size 4 on MPS

---
+++ collect_mps.sh
@@ -0,0 +1,38 @@
+#!/bin/bash 
+
+# Retrieve the starting line number from the command-line argument
+start_line="$1"
+device_id="$2"
+
+# Check if both arguments are provided and valid
+if [[ -z "$start_line" || ! "$start_line" =~ ^[0-9]+$ ]] || [[ ! "$device_id" =~ ^[0-3]$ ]]; then
+    echo "Usage: $0 <starting_line_number> <device_id (0-3)>" >&2
+    exit 1
+fi
+
+input_file="./combos_4.txt"
+
+# Read lines starting from the specified line and ending 60 lines later
+counter=0
+while IFS= read -r line && [ $counter -lt 350 ]; do
+    ((counter++))
+
+    result_line=""
+
+    read -ra parts <<< "$line"
+    for part in "${parts[@]}"; do
+        IFS='-' read -ra substrings <<< "$part"
+        concatenated="${substrings[0]}-${substrings[1]}"
+        result_line="$result_line vision-$concatenated"
+    done
+    model_mixes+=("$result_line")
+done < <(sed -n "${start_line},+350p" "$input_file")
+
+for model_mix in "${model_mixes[@]}"
+do
+    # ./run.sh --device-type a100 --device-id 0 --modes mps-uncap --distribution closed ${model_mix}
+    cmd="./run.sh --device-type a100 --device-id ${device_id} --modes mps-uncap --duration 60 --distribution closed ${model_mix}"
+    echo "${model_mix}"
+    echo "${cmd}"
+    # eval $cmd
+done
