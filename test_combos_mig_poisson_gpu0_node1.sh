538474c85dcf1c78d8d9f38705e2b86e44ac6e63
Prasoon Sinha <prasoon.sinha@utexas.edu>
2024-03-24 15:37:08 +0000

    Add mig poisson test combos scripts

---
+++ test_combos_mig_poisson_gpu0_node1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+
+models=('mobilenet_v2' 'mobilenet_v3_small' 'mobilenet_v3_large' 'densenet121')
+# models=('densenet161' 'densenet169' 'densenet201' 'vgg11')
+# models=('vgg13' 'vgg16' 'vgg19')
+# models=('inception_v3' 'resnet18' 'resnet34' )
+# models=('resnet50' 'resnet101' 'resnet152')
+# models=('alexnet' 'squeezenet1_0' 'squeezenet1_1' )
+# models=('efficientnet_b5' 'efficientnet_b6' 'efficientnet_b7')
+batches=(2 4 8 16 32)
+
+# First run with 4 and 3
+for model in "${models[@]}"; do
+    for batch in "${batches[@]}"; do
+        command="./run.sh --device-type a100 --device-id 0 --modes mig --duration 60 --distribution poisson --load-start 0.1 --load-end 1.0 vision-${model}-${batch} vision-${model}-${batch}"
+        echo "$command"
+        eval $command
+    done
+done
+
+# Second run with 2, 2, 2, 1
+for model in "${models[@]}"; do
+    for batch in "${batches[@]}"; do
+        command="./run.sh --device-type a100 --device-id 0 --modes mig --duration 60 --distribution poisson --load-start 0.1 --load-end 1.0 vision-${model}-${batch} vision-${model}-${batch} vision-${model}-${batch} vision-${model}-${batch}"
+        echo "$command"
+        eval $command
+    done
+done
